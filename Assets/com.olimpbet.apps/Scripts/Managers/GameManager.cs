da0d66650a55bab41e5d69ee3a0fa366d84a846f
SGCEntertainment <nikcxprojects@gmail.com>
2023-04-22 20:00:20 +0300

    init

--- Assets/com.olimpbet.apps/Scripts/Managers/GameManager.cs
+++ Assets/com.olimpbet.apps/Scripts/Managers/GameManager.cs
@@ -4,7 +4,65 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Color color;
+    private Coroutine drawing;
+
     [SerializeField] Transform outline;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var hit = Physics2D.Raycast(origin, Vector2.zero, Mathf.Infinity);
+            if(hit.collider != null && hit.collider.CompareTag("start"))
+            {
+                color = hit.collider.GetComponent<Ball>().color;
+                StartLine();
+            }
+        }
+        
+        if(drawing != null && Input.GetMouseButtonUp(0))
+        {
+            FinishLine();
+        }
+    }
+
+    private void StartLine()
+    {
+        if(drawing != null)
+        {
+            StopCoroutine(drawing);
+        }
+
+        drawing = StartCoroutine(nameof(DrawLine));
+    }
+
+    private void FinishLine()
+    {
+        StopCoroutine(drawing);
+    }
+
+    private IEnumerator DrawLine()
+    {
+        var linePrefab = Resources.Load<LineRenderer>("line");
+        var line = Instantiate(linePrefab);
+
+
+        line.startColor = line.endColor = color;
+        line.positionCount = 0;
+
+        while(true)
+        {
+            var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            position.z = 0;
+
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, position);
+
+            yield return null;
+        }
+    }
 
     public void SelectLevel(int levelId)
     {
--- Assets/com.olimpbet.apps/Scripts/Managers/UIManager.cs
+++ Assets/com.olimpbet.apps/Scripts/Managers/UIManager.cs
@@ -16,9 +16,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject game;
     [SerializeField] GameObject levels;
 
-
     [Space(10)]
-    [SerializeField] AudioSource sfxSource;
+    [SerializeField] Text levelText;
 
 
     private void Start()
@@ -41,36 +40,17 @@ public class UIManager : MonoBehaviour
             Destroy(_gameRef);
         }
 
+        levelText.text = $"LVL {levelId}";
+
         var _parent = GameObject.Find("Environment").transform;
         var _prefab = Resources.Load<GameObject>($"level {levelId}");
 
         _gameRef = Instantiate(_prefab, _parent);
 
         menu.SetActive(false);
+        levels.SetActive(false);
+
         game.SetActive(true);
-    }
-
-    public void Stop()
-    {
-        var greenLine = GameObject.Find("green line").transform;
-        var indicator = GameObject.Find("indicator").transform;
-
-        var distance = Vector2.Distance(greenLine.position, indicator.position);
-        var IsTrue = distance <= 0.562f;
-
-        if(!IsTrue)
-        {
-            var expl = Instantiate(Resources.Load<GameObject>("explosion"));
-            Destroy(expl, 1.0f);
-        }
-
-        var popupResult = IsTrue ? "cool" : "lose";
-        var prefab = Resources.Load<GameObject>(popupResult);
-
-        var popup = Instantiate(prefab);
-        Destroy(popup, 1.0f);
-
-        StartGame();
     }
 
     public void OpenMenu()
@@ -78,6 +58,12 @@ public class UIManager : MonoBehaviour
         if(_gameRef)
         {
             Destroy(_gameRef);
+        }
+
+        var lines = FindObjectsOfType<LineRenderer>();
+        foreach (var line in lines)
+        {
+            Destroy(line.gameObject);
         }
 
         levels.SetActive(false);
